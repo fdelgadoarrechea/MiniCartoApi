33541c8d49ce4d650fd0a62fbef1fc0107a04b8b
Francisco Delgado Arrechea <fdelgadoarrechea@gmail.com>
2017-09-19 00:48:51 +0200

    fixed eslint problems

--- src/index.js
+++ src/index.js
@@ -6,36 +6,36 @@ import MapRenderer from './maprenderer.js';
 let config = new ConfigReader(mapconfig);
 
 function validateResponse(response) {
-    if (!response.ok) {
-        throw Error(response.statusText);
-    }
-    return response;
+  if (!response.ok) {
+    throw Error(response.statusText);
+  }
+  return response;
 }
 
 function readResponseAsJSON(response) {
-    return response.json();
+  return response.json();
 }
 
 function renderResult(mapsAPIresponse) {
-    let cartoLayer = new CartoTemplate(mapsAPIresponse, config.getUser());
+  let cartoLayer = new CartoTemplate(mapsAPIresponse, config.getUser());
 
-    config.pushCartoLayer(cartoLayer.getTemplateURL());
-    let renderer = new MapRenderer(config.getCenter(), config.getZoom());
+  config.pushCartoLayer(cartoLayer.getTemplateURL());
+  let renderer = new MapRenderer(config.getCenter(), config.getZoom());
 
-    renderer.renderMap('mapid');
-    renderer.renderLayers(config.getLayers());
+  renderer.renderMap('mapid');
+  renderer.renderLayers(config.getLayers());
 }
 
 function logError(error) {
-    console.log('Looks like there was a problem: \n', error);
+  console.log('Looks like there was a problem: \n', error);
 }
 
 fetch(config.getURL(), {
-    mode: 'cors',
-    method: 'POST',
-    body: JSON.stringify(config.getMapConfig()),
-    headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-    }
+  mode: 'cors',
+  method: 'POST',
+  body: JSON.stringify(config.getMapConfig()),
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  }
 }).then(validateResponse).then(readResponseAsJSON).then(renderResult).catch(logError);
--- src/maprenderer.js
+++ src/maprenderer.js
@@ -1,22 +1,22 @@
 export default class MapRenderer {
-    constructor(center, zoom) {
-        this._center = center;
-        this._zoom = zoom;
-        this._templateURL = '';
-        this._currentMap = '';
-    }
+  constructor(center, zoom) {
+    this._center = center;
+    this._zoom = zoom;
+    this._templateURL = '';
+    this._currentMap = '';
+  }
 
-    renderMap(divid) {
-        this._currentMap = L.map(divid).setView(this._center, this._zoom);
-    }
+  renderMap(divid) {
+    this._currentMap = L.map(divid).setView(this._center, this._zoom);
+  }
 
-    renderLayers(layers) {
-        let map = this._currentMap;
+  renderLayers(layers) {
+    let map = this._currentMap;
 
-        layers.forEach(function (element) {
-            L.tileLayer(element, {
-                maxZoom: 18
-            }).addTo(map);
-        });
-    }
+    layers.forEach(function (element) {
+      L.tileLayer(element, {
+        maxZoom: 18
+      }).addTo(map);
+    });
+  }
 }
